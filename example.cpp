d41b1a7471b1e8291301c70c990ebb741d7b7ff6
Jomo Fisher <jomof@google.com>
2017-03-21 11:18:45 -0700

    .

---
+++ example.cpp
@@ -0,0 +1,11 @@
+#include <ft2build.h>
+#include FT_FREETYPE_H
+int main() {
+  FT_Library library;
+  FT_Error error;
+  error = FT_Init_FreeType(&library);
+  if (error) {
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
